3fcf38bddd06c8f123368cc712685b814c946ed5
wale-prog <78151836+wale-prog@users.noreply.github.com>
2021-11-25 15:09:33 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var btn = document.querySelector(".mobile-menu-button");
+var sidebar = document.querySelector(".sidebar");
+btn.addEventListener("click", () => {
+    sidebar.classList.toggle("-translate-y-full")
+ }) 
+
+
